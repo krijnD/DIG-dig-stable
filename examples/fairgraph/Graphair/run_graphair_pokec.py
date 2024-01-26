961b033068521585ce3e1add2132d59aaede0302
krijnD <krijnfelipedignum@gmail.com>
2024-01-26 22:17:54 +0100

    updated values to be consistent with the paper
    - hyperparameters also consistent with hpo

--- examples/fairgraph/Graphair/run_graphair_pokec.py
+++ examples/fairgraph/Graphair/run_graphair_pokec.py
@@ -3,7 +3,7 @@ from dig.fairgraph.dataset import POKEC, NBA
 import torch
 
 # Load the dataset and split
-pokec = POKEC(dataset_sample='pockec_z') # you may also choose 'pockec_n'
+pokec = POKEC(dataset_sample='pockec_n') # you may also choose 'pockec_n'
 
 # Print the current working directory
 import os
@@ -12,5 +12,5 @@ print("Current working directory:", os.getcwd())
 # Train and evaluate
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 run_fairgraph = run()
-run_fairgraph.run(device,dataset=pokec,model='Graphair',epochs=2000,batch_size=1000,
+run_fairgraph.run(device,dataset=pokec,model='Graphair',epochs=500,batch_size=1000,
             lr=1e-4,weight_decay=1e-5)
